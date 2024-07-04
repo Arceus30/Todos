d208a10cb8db569ec59ef53d58f0909fe1de5519
Arceus30 <keshavkhandelwal30@gmail.com>
2024-07-04 16:54:02 +0530

    First Commit

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Count from "./Count.jsx";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/count",
+        element: <Count />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={routes} />
+    </React.StrictMode>
+);
