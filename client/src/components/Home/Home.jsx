1d534794065a6e9756caf462a0638ab01e98cf2e
Arceus30 <keshavkhandelwal30@gmail.com>
2024-07-04 19:12:15 +0530

    Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "./Home.css";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { toast } from "react-toastify";
 
 const Home = () => {
-    return <div>Home</div>;
+    //     const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+    //     const userId = useSelector((state) => state.user.userId);
+    //     const navigate = useNavigate();
+    const handleClick = () => {
+        //         if (isLoggedIn || userId) {
+        //             navigate(import.meta.env.VITE_TODO);
+        //         }
+        //         toast.error("You need to login first");
+        //         navigate(import.meta.env.VITE_SIGNIN);
+    };
+    return (
+        <div className="d-flex my-auto justify-content-center align-items-center mb-3">
+            <div className="w-75 d-flex justify-content-center align-items-center flex-column mx-0">
+                <h1 className="text-center customHead1">
+                    Organize your <br />
+                    work and life, finally.
+                </h1>
+                <p className="text-center my-2 customText1">
+                    Become focused, organized, and calm with <br />
+                    todo app. The World's #1 task manager app.
+                </p>
+                <button
+                    className="btn btn-danger my-3 p-2 fs-3 fw-semibold"
+                    onClick={handleClick}
+                >
+                    Make Todo List
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
